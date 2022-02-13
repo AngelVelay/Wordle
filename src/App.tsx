7f699688fa2f719abd87e635438ad1ca452518ce
Angel Velay <velayangel@gmail.com>
2022-02-13 05:35:54 -0600

    Version 1.0

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Title } from "./components/Title";
+import Wordle from "./components/Wordle";
+
+function App() {
+  return (
+    <>
+      <Title />
+      <Wordle />
+    </>
+  );
+}
+
+export default App;
